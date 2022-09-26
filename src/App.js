18251b26848c4eccf9026b1bf591cad14d993407
권윤아(YoonAh Kwon)/Media솔루션Platform팀/SKP <yakwon@sk.com>
2022-09-26 17:32:23 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React I LOVE YUNTAEK
+          Learn React I LOVE YUNTAEK much
         </a>
       </header>
     </div>
